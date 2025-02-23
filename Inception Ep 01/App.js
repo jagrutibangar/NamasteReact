ef5c08c306341524da078d4c4650f3cdc86e287e
jagrutibangar <161784351+jagrutibangar@users.noreply.github.com>
2025-02-23 15:42:44 +0530

    Add files via upload

---
+++ Inception Ep 01/App.js
@@ -0,0 +1,33 @@
+//html element using react
+const heading = React.createElement(
+    "h1", 
+    {id:"head"}, 
+    "Hello World from React!");
+
+console.log(heading);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(heading);
+
+//another object in react
+const subhead = React.createElement("h2", {id:"subhead"}, "This is another component made using react!")
+const root2 = ReactDOM.createRoot(document.getElementById("root2"));
+root2.render(subhead);
+
+// Forming a nested structure of divs using react
+
+const parent = React.createElement(
+        "div" , 
+        {id:"parent"}, 
+    React.createElement(
+        "div",
+        {id:"child"},
+    [React.createElement("h1", "This is statement is passed in a nested h1 tag"),
+    React.createElement("h2", {}, "This is a sibling of the above element")]
+    )
+);
+
+//This react element creates an HTML object which the browser understands
+
+const a = ReactDOM.createRoot(document.getElementById("newid"));
+
+a.render(parent); //render functions converts "parent" it into an HTML object
