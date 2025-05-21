4c296c327c48ffed6ceaae0664fc4a1bac1ad439
jagrutibangar <161784351+jagrutibangar@users.noreply.github.com>
2025-05-21 21:32:28 +0530

    Update Shimmer.jsx

--- SwiggyClone/components/Shimmer.jsx
+++ SwiggyClone/components/Shimmer.jsx
@@ -1,18 +1,26 @@
 import React from 'react'
-import './Shimmer.css' //default exports
+//import './Shimmer.css' //default exports
 
 const Shimmer = () => {
   return(
-    <>
-    <div className='shimmer-container'>
-    <div className='shimmer-card'></div>
-    <div className='shimmer-card'></div>
-    <div className='shimmer-card'></div>
-    <div className='shimmer-card'></div>
-    <div className='shimmer-card'></div>
-    <div className='shimmer-card'></div>
-    <div className='shimmer-card'></div>
-    <div className='shimmer-card'></div>
+    <> 
+    <div className='mt-4 w-full p-5 bg-gray-50'>
+    <div className='border-1 rounded-2xl h-10 w-50 ml-60 mt-10 bg-orange-50'></div>
+    <div className='border-1 rounded-4xl h-10 w-100 bg-gray-100 ml-130 -mt-10'></div>
+    <div className='border-1 rounded-2xl h-10 w-100 bg-gray-100 ml-130 -mt-10'></div>
+    <div className='border-1 rounded-2xl h-10 w-20 bg-orange-100 ml-232 -mt-10'></div>
+    
+    <div className='flex flex-wrap mt-4 w-screen bg-gray-50 p-5 items-center ml-15'>
+    <div className='border-1 h-90 w-70 mt-1 rounded-4xl space-x-1 m-2'></div>
+    <div className='border-1 h-90 w-70 mt-1 rounded-4xl ml-2 bg-gray-100'></div>
+    <div className='border-1 h-90 w-70 mt-1 rounded-4xl ml-2 bg-gray-100'></div>
+    <div className='border-1 h-90 w-70 mt-1 rounded-4xl ml-2 bg-gray-100'></div>
+    <div className='border-1 h-90 w-70 mt-1 rounded-4xl ml-2 bg-gray-100'></div>
+    <div className='border-1 h-90 w-70 mt-1 rounded-4xl ml-2 bg-gray-100'></div>
+    <div className='border-1 h-90 w-70 mt-1 rounded-4xl ml-2 bg-gray-100'></div>
+    <div className='border-1 h-90 w-70 mt-1 rounded-4xl ml-2 bg-gray-100'></div>
+    </div>
+
     </div>
     </>
   )
