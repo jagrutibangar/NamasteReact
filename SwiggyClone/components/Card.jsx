d64c66b147b7c008eb7488ee3467a1815ba67fe4
jagrutibangar <161784351+jagrutibangar@users.noreply.github.com>
2025-05-21 21:31:07 +0530

    Update Card.jsx

--- SwiggyClone/components/Card.jsx
+++ SwiggyClone/components/Card.jsx
@@ -1,5 +1,4 @@
 import React from 'react'
-import './Card.css'
 
 const Card = ({ resData }) => {
   const {
@@ -10,17 +9,37 @@ const Card = ({ resData }) => {
     sla: { deliveryTime },
   } = resData.info;
 
+
+  
   return (
     <>
-      <div className="res-card">
-        <img id="res-image" src={`https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/${cloudinaryImageId}`}alt="" />
-        <h3>{name}</h3>
-        <h4>{cuisines.join(', ')}</h4>
-        <h5>{avgRating} Stars</h5>
-        <h4>{deliveryTime} minutes</h4>
+      <div className="w-70 h-115 sm:w-70 rounded-4xl mt-10 m-2 bg-white shadow-lg hover:bg-gray-200 xl transition-shadow duration-300 ease-in-out ">
+        <img className="w-full h-70 rounded-4xl " src={`https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/${cloudinaryImageId}`} />
+        <h3 className='p-1 font-semibold'>{name}</h3>
+        <h4 className='p-1'>{cuisines.join(', ')}</h4>
+        <h5 className='p-1'>{avgRating} Stars</h5>
+        <h4 className='p-1'>{deliveryTime} minutes</h4>
       </div>
     </>
   )
+};
+
+export { Card };
+
+// Higher Order Component (HOC) to add a "Open" label
+export const withOpenLabel = (Card) => {
+  return (props) => {
+    return(
+      <>
+      <div>
+      <label className='bg-black absolute text-white rounded-lg border-amber-500 font-sans'> Open </label>
+      <Card {...props}></Card>
+      </div>
+      </>
+    );
+  }
 }
 
-export default Card;
+const PromotedCard = withOpenLabel(Card);
+
+export default PromotedCard;
